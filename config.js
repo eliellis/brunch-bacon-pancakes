d34a12d71ed92c598d1034aaccf96286aeaa761a
Eli Ellis <me@eliellis.com>
2013-05-12 00:54:07 -0400

    readme, config

--- config.js
+++ config.js
@@ -1,49 +1,49 @@
 exports.config = {
-    paths: {
-      "public": 'public'
-    },
-    files: {
-      javascripts: {
-        defaultExtension: 'js',
-        joinTo: {
-          'javascripts/app.js': /^app/,
-          'javascripts/vendor.js': /^vendor/
-        },
-        order: {
-          before:
-          [
-            'vendor/scripts/console-helper.js',
-            'vendor/scripts/jquery-1.9.1.js',
-            'vendor/scripts/underscore-1.4.4.js',
-            'vendor/scripts/backbone-1.0.0.js',
-            'vendor/scripts/backbone-mediator.js',
-            'vendor/scripts/bootstrap/bootstrap-tooltip.js',
-            'vendor/scripts/bootstrap/bootstrap-transition.js',
-            'vendor/scripts/bootstrap/bootstrap-affix.js',
-            'vendor/scripts/bootstrap/bootstrap-alert.js',
-            'vendor/scripts/bootstrap/bootstrap-button.js',
-            'vendor/scripts/bootstrap/bootstrap-carousel.js',
-            'vendor/scripts/bootstrap/bootstrap-collapse.js',
-            'vendor/scripts/bootstrap/bootstrap-dropdown.js',
-            'vendor/scripts/bootstrap/bootstrap-modal.js',
-            'vendor/scripts/bootstrap/bootstrap-popover.js',
-            'vendor/scripts/bootstrap/bootstrap-scrollspy.js',
-            'vendor/scripts/bootstrap/bootstrap-tab.js',
-            'vendor/scripts/bootstrap/bootstrap-typeahed.js'
+  paths: {
+    "public": 'public'
+  },
+  files: {
+    javascripts: {
+      defaultExtension: 'js',
+      joinTo: {
+        'javascripts/app.js': /^app/,
+        'javascripts/vendor.js': /^vendor/
+      },
+      order: {
+        before:
+        [
+          'vendor/scripts/console-helper.js',
+          'vendor/scripts/jquery-1.9.1.js',
+          'vendor/scripts/underscore-1.4.4.js',
+          'vendor/scripts/backbone-1.0.0.js',
+          'vendor/scripts/backbone-mediator.js',
+          'vendor/scripts/bootstrap/bootstrap-tooltip.js',
+          'vendor/scripts/bootstrap/bootstrap-transition.js',
+          'vendor/scripts/bootstrap/bootstrap-affix.js',
+          'vendor/scripts/bootstrap/bootstrap-alert.js',
+          'vendor/scripts/bootstrap/bootstrap-button.js',
+          'vendor/scripts/bootstrap/bootstrap-carousel.js',
+          'vendor/scripts/bootstrap/bootstrap-collapse.js',
+          'vendor/scripts/bootstrap/bootstrap-dropdown.js',
+          'vendor/scripts/bootstrap/bootstrap-modal.js',
+          'vendor/scripts/bootstrap/bootstrap-popover.js',
+          'vendor/scripts/bootstrap/bootstrap-scrollspy.js',
+          'vendor/scripts/bootstrap/bootstrap-tab.js',
+          'vendor/scripts/bootstrap/bootstrap-typeahed.js'
           ]
-        }
-      },
-      stylesheets: {
-        defaultExtension: 'less',
-        joinTo: 'stylesheets/app.css',
-        order: {
-          before: ['vendor/styles/bootstrap/bootstrap.less']
-        }
-      },
-      templates: {
-        defaultExtension: 'hbs',
-        joinTo: 'javascripts/app.js'
       }
     },
-    framework: 'backbone'
+    stylesheets: {
+      defaultExtension: 'less',
+      joinTo: 'stylesheets/app.css',
+      order: {
+        before: ['vendor/styles/bootstrap/bootstrap.less']
+      }
+    },
+    templates: {
+      defaultExtension: 'hbs',
+      joinTo: 'javascripts/app.js'
+    }
+  },
+  framework: 'backbone'
 };
